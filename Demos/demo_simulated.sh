f0a711d6f754dbdc6494732d577583a81bdd73cf
Calvin McCarter <calvin199@gmail.com>
2020-03-18 00:44:07 -0500

    added smaller datasets for demo_simulated.sh

--- Demos/demo_simulated.sh
+++ Demos/demo_simulated.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-../Mega-sCGGM/mega_scggm -y 3.2 -x 0.97 100 10000 100 20000 ../Examples/simulated_Y_n100_p20000_q10000.txt ../Examples/simulated_X_n100_p20000.txt simulatedLambda simulatedTheta simulatedStats
-../Fast-sCGGM/fast_scggm -y 3.2 -x 0.97 100 10000 100 20000 ../Examples/simulated_Y_n100_p20000_q10000.txt ../Examples/simulated_X_n100_p20000.txt simLambda simTheta simStats
+../Mega-sCGGM/mega_scggm -y 3.2 -x 0.97 100 1000 100 2000 ../Examples/simulated_Y_n100_p2000_q1000.txt ../Examples/simulated_X_n100_p2000.txt simulatedLambda simulatedTheta simulatedStats
+../Fast-sCGGM/fast_scggm -y 3.2 -x 0.97 100 1000 100 2000 ../Examples/simulated_Y_n100_p2000_q1000.txt ../Examples/simulated_X_n100_p2000.txt simLambda simTheta simStats
